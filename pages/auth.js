e10828e6bf523a9def1b1264fd1732bf33208027
Uchenna Ihieri <uchennaihieri@yahoo.com>
2023-02-22 08:31:43 +0100

    update changes

--- pages/auth.js
+++ pages/auth.js
@@ -27,7 +27,9 @@ import { Bree_Serif, DM_Sans, Roboto_Slab } from '@next/font/google';
 import { useEffect, useState } from 'react';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { RiEyeCloseFill, RiEyeFill } from 'react-icons/ri';
+import { useForm } from "react-hook-form";
 import { useRouter } from 'next/router'
+import { auth, createUserWithEmailAndPassword, db, doc, setDoc } from '@/services/firebase';
 
 const dmsansBold = DM_Sans({ weight: '500', subsets: ['latin'] })
 const bree = Bree_Serif({ weight: '400', subsets: ['latin'] })
@@ -38,6 +40,8 @@ const robotoItalic = DM_Sans({ weight: '400', style: 'italic', subsets: ['latin'
 export default function Auth() {
 
     const [page, setPage] = useState(0)
+    const { register, handleSubmit } = useForm();
+
 
     const router = useRouter()
 
@@ -62,23 +66,23 @@ export default function Auth() {
         setPage(index)
     }
 
+
     return (
         <Flex
-            key={router.asPath}
             flexDirection={'column'}
             minH={'100vh'}
             align={'center'}
             bg={'#FFFFFF'}>
             <Flex
-                mt="2.875rem"
-                width="87.5rem"
+                mt={{ base: '1.5rem', md: "2.875rem" }}
+                width={{ base: '90%', md: "87.5rem" }}
                 flexDir={'row'}
                 justify="space-between"
-                mb={'10.6rem'}
+                mb={{ base: '3.5rem', md: '10.6rem' }}
             >
                 <Button leftIcon={<AiOutlineLeft />} m="0" p="0" variant={'ghost'} _hover={{
                     backgroundColor: 'none'
-                }} color={'#000000'} fontSize="1.44rem"
+                }} color={'#000000'} fontSize={{ base: '1rem', md: "1.44rem" }}
                     fontWeight={400}
                     lineHeight={'4rem'}
                     className={bree.className}
@@ -87,12 +91,12 @@ export default function Auth() {
 
                 <Button m="0" p="0" variant={'ghost'} _hover={{
                     backgroundColor: 'none'
-                }} color={'#000000'} fontSize="1rem"
+                }} color={'#000000'} fontSize={{ base: '0.75rem', md: "1rem" }}
                     fontWeight={500}
                     className={dmsansBold.className}
                 >Support</Button>
             </Flex>
-            <Box mb="5.06rem">
+            <Box mb={{ base: '2.75rem', md: "5.06rem" }}>
                 <Image src={'/footerlogo.png'} w={'14.17rem'} h={'3.06rem'} />
             </Box>
 
@@ -151,7 +155,7 @@ export const Signin = () => {
         <Box
             m="0px"
             borderRadius={'2px'}
-            w="37.93rem"
+            w={{ base: "27.93rem", md: "37.93rem" }}
             h="35.37rem"
             bg={'#F9FAFC'}
             border={'1px solid rgba(0, 0, 0, 0.14);'}
@@ -261,110 +265,148 @@ export const Signin = () => {
 
 
 export const Signup = () => {
-
+    const { register, handleSubmit } = useForm();
+    const [loadingState, setLoadingState] = useState(false);
     const [show, setShow] = useState(false)
     const handleClick = () => setShow(!show)
+
+
+
+    const onSubmit = (data) => {
+        setLoadingState(true)
+
+        createUserWithEmailAndPassword(auth, data.email, data.pwd)
+            .then(async (userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user)
+
+                await setDoc(doc(db, "users", `${user.uid}`), {
+                    firstName: `${data.firstName}`,
+                    lastName: `${data.lastName}`,
+                    email: `${data.email}`,
+                    phoneNumber: `${data.phoneNumber}`
+                });
+                // ...
+
+                setLoadingState(false)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+
+                console.log(errorCode)
+                console.log(errorMessage)
+                // ..
+                setLoadingState(false)
+            });
+    }
+
 
     return (
 
         <Box
             m="0px"
             borderRadius={'2px'}
-            w="37.93rem"
+            w={{ base: "27.93rem", md: "37.93rem" }}
             h="55.37rem"
             bg={'#F9FAFC'}
             border={'1px solid rgba(0, 0, 0, 0.14);'}
             py={'4.5rem'} px={'4.8rem'}>
-            <Stack >
-                <Box mb="1.6rem">
-                    <FormControl id="email" >
-                        <FormLabel className={roboto.className}>First Name</FormLabel>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Stack >
+                    <Box mb="1.6rem">
+                        <FormControl id="email" >
+                            <FormLabel className={roboto.className}>First Name</FormLabel>
 
-                        <Input h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="email" placeholder='Legal first name' />
+                            <Input {...register("firstName")} h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="text" placeholder='Legal first name' />
 
-                    </FormControl>
-                </Box>
+                        </FormControl>
+                    </Box>
 
-                <Box >
-                    <FormControl id="email" >
-                        <FormLabel className={roboto.className}>Last Name</FormLabel>
+                    <Box >
+                        <FormControl id="email" >
+                            <FormLabel className={roboto.className}>Last Name</FormLabel>
 
-                        <Input mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="email" placeholder='Legal last name' />
+                            <Input {...register("lastName")} mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="text" placeholder='Legal last name' />
 
-                    </FormControl>
-                </Box>
-                <Box >
-                    <FormControl id="email" >
-                        <FormLabel className={roboto.className}>Email</FormLabel>
+                        </FormControl>
+                    </Box>
+                    <Box >
+                        <FormControl id="email" >
+                            <FormLabel className={roboto.className}>Email</FormLabel>
 
-                        <Input mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="email" placeholder='Enter your email' />
+                            <Input {...register("email")} mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="email" placeholder='Enter your email' />
 
-                    </FormControl>
-                </Box>
-                <Box >
-                    <FormControl id="email" >
-                        <FormLabel className={roboto.className}>Phone Number</FormLabel>
+                        </FormControl>
+                    </Box>
+                    <Box >
+                        <FormControl id="email" >
+                            <FormLabel className={roboto.className}>Phone Number</FormLabel>
 
-                        <Input mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="email" placeholder='Enter your active phone number' />
+                            <Input {...register("phoneNumber")} mb="1.6rem" h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'} type="number" placeholder='Enter your active phone number' />
 
-                    </FormControl>
-                </Box>
+                        </FormControl>
+                    </Box>
 
-                <Box
-                >
-                    <FormControl id="password" >
-                        <FormLabel className={roboto.className}>Password</FormLabel>
+                    <Box
+                    >
+                        <FormControl id="password" >
+                            <FormLabel className={roboto.className}>Password</FormLabel>
 
-                        <InputGroup mb="2.6rem">
-                            <InputLeftElement
-                                h="100%"
-                                pointerEvents='none'
-                            >
-                                <Icon>
-                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <g opacity="0.2" clipPath="url(#clip0_79_274)">
-                                            <path d="M18 8H20C20.2652 8 20.5196 8.10536 20.7071 8.29289C20.8946 8.48043 21 8.73478 21 9V21C21 21.2652 20.8946 21.5196 20.7071 21.7071C20.5196 21.8946 20.2652 22 20 22H4C3.73478 22 3.48043 21.8946 3.29289 21.7071C3.10536 21.5196 3 21.2652 3 21V9C3 8.73478 3.10536 8.48043 3.29289 8.29289C3.48043 8.10536 3.73478 8 4 8H6V7C6 5.4087 6.63214 3.88258 7.75736 2.75736C8.88258 1.63214 10.4087 1 12 1C13.5913 1 15.1174 1.63214 16.2426 2.75736C17.3679 3.88258 18 5.4087 18 7V8ZM11 15.732V18H13V15.732C13.3813 15.5119 13.6793 15.1721 13.8478 14.7653C14.0162 14.3586 14.0458 13.9076 13.9319 13.4823C13.8179 13.057 13.5668 12.6813 13.2175 12.4132C12.8682 12.1452 12.4403 11.9999 12 11.9999C11.5597 11.9999 11.1318 12.1452 10.7825 12.4132C10.4332 12.6813 10.1821 13.057 10.0681 13.4823C9.9542 13.9076 9.98376 14.3586 10.1522 14.7653C10.3207 15.1721 10.6187 15.5119 11 15.732ZM16 8V7C16 5.93913 15.5786 4.92172 14.8284 4.17157C14.0783 3.42143 13.0609 3 12 3C10.9391 3 9.92172 3.42143 9.17157 4.17157C8.42143 4.92172 8 5.93913 8 7V8H16Z" fill="black" />
-                                        </g>
-                                        <defs>
-                                            <clipPath id="clip0_79_274">
-                                                <rect width="24" height="24" fill="white" />
-                                            </clipPath>
-                                        </defs>
-                                    </svg>
-                                </Icon>
+                            <InputGroup mb="2.6rem">
+                                <InputLeftElement
+                                    h="100%"
+                                    pointerEvents='none'
+                                >
+                                    <Icon>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g opacity="0.2" clipPath="url(#clip0_79_274)">
+                                                <path d="M18 8H20C20.2652 8 20.5196 8.10536 20.7071 8.29289C20.8946 8.48043 21 8.73478 21 9V21C21 21.2652 20.8946 21.5196 20.7071 21.7071C20.5196 21.8946 20.2652 22 20 22H4C3.73478 22 3.48043 21.8946 3.29289 21.7071C3.10536 21.5196 3 21.2652 3 21V9C3 8.73478 3.10536 8.48043 3.29289 8.29289C3.48043 8.10536 3.73478 8 4 8H6V7C6 5.4087 6.63214 3.88258 7.75736 2.75736C8.88258 1.63214 10.4087 1 12 1C13.5913 1 15.1174 1.63214 16.2426 2.75736C17.3679 3.88258 18 5.4087 18 7V8ZM11 15.732V18H13V15.732C13.3813 15.5119 13.6793 15.1721 13.8478 14.7653C14.0162 14.3586 14.0458 13.9076 13.9319 13.4823C13.8179 13.057 13.5668 12.6813 13.2175 12.4132C12.8682 12.1452 12.4403 11.9999 12 11.9999C11.5597 11.9999 11.1318 12.1452 10.7825 12.4132C10.4332 12.6813 10.1821 13.057 10.0681 13.4823C9.9542 13.9076 9.98376 14.3586 10.1522 14.7653C10.3207 15.1721 10.6187 15.5119 11 15.732ZM16 8V7C16 5.93913 15.5786 4.92172 14.8284 4.17157C14.0783 3.42143 13.0609 3 12 3C10.9391 3 9.92172 3.42143 9.17157 4.17157C8.42143 4.92172 8 5.93913 8 7V8H16Z" fill="black" />
+                                            </g>
+                                            <defs>
+                                                <clipPath id="clip0_79_274">
+                                                    <rect width="24" height="24" fill="white" />
+                                                </clipPath>
+                                            </defs>
+                                        </svg>
+                                    </Icon>
 
-                            </InputLeftElement>
+                                </InputLeftElement>
 
-                            <Input h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'}
-                                type={show ? 'text' : 'password'} placeholder='Enter your password' />
-                            <InputRightElement h="100%" mx="1rem">
-                                {show ?
-                                    <Icon _hover={{
-                                        cursor: 'pointer'
-                                    }} as={RiEyeFill} fontSize="1.5rem" color="#9A9C9D" onClick={handleClick} /> : <Icon _hover={{
-                                        cursor: 'pointer'
-                                    }} as={RiEyeCloseFill} fontSize="1.5rem" color="#9A9C9D" onClick={handleClick} />}
-                            </InputRightElement>
-                        </InputGroup>
-                    </FormControl>
-                </Box>
+                                <Input {...register("pwd")} h="3.6rem" borderRadius={'0.125rem'} bg="#ffffff" border={'1px solid #D5D6D6'}
+                                    type={show ? 'text' : 'password'} placeholder='Enter your password' />
+                                <InputRightElement h="100%" mx="1rem">
+                                    {show ?
+                                        <Icon _hover={{
+                                            cursor: 'pointer'
+                                        }} as={RiEyeFill} fontSize="1.5rem" color="#9A9C9D" onClick={handleClick} /> : <Icon _hover={{
+                                            cursor: 'pointer'
+                                        }} as={RiEyeCloseFill} fontSize="1.5rem" color="#9A9C9D" onClick={handleClick} />}
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                    </Box>
 
-                <Button
+                    <Button
+                        type='submit'
+                        isLoading={loadingState}
+                        loadingText='Please wait..'
+                        borderRadius={'0.125rem'}
+                        h="4rem"
+                        bg={'#000000'}
+                        color={'white'}
+                        _hover={{
+                            bg: 'black.400',
+                        }}>
+                        <Text
+                            fontSize="1.5rem" className={roboto.className} >
+                            Register
+                        </Text>
 
-                    borderRadius={'0.125rem'}
-                    h="4rem"
-                    bg={'#000000'}
-                    color={'white'}
-                    _hover={{
-                        bg: 'black.400',
-                    }}>
-                    <Text
-                        fontSize="1.5rem" className={roboto.className} >
-                        Register
-                    </Text>
-
-                </Button>
-            </Stack>
+                    </Button>
+                </Stack>
+            </form>
         </Box>
     )
 }
---
+++ pages/pricing.js
@@ -0,0 +1,119 @@
+import Navbar from '@/components/navbar';
+import {
+    Box,
+    Button,
+    Divider,
+    Heading,
+    List,
+    ListIcon,
+    ListItem,
+    Stack,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { FaCheckCircle } from 'react-icons/fa';
+
+const options = [
+    { id: 1, desc: '1 lorem ipsum' },
+    { id: 2, desc: 'Lorem, ipsum dolor.' },
+    { id: 3, desc: 'Monthly Updates' },
+];
+
+const PackageTier = ({ checked, typePlan, title }) => {
+
+    const colorTextLight = checked ? 'white' : 'purple.600';
+    const bgColorLight = checked ? 'purple.400' : 'gray.300';
+
+    const colorTextDark = checked ? 'white' : 'purple.500';
+    const bgColorDark = checked ? 'purple.400' : 'gray.300';
+
+    return (
+        <Stack
+            p={3}
+            py={3}
+            justifyContent={{
+                base: 'flex-start',
+                md: 'space-around',
+            }}
+            direction={{
+                base: 'column',
+                md: 'row',
+            }}
+            alignItems={{ md: 'center' }}>
+            <Heading color={'#ffffff'} size={'xl'}>{title}</Heading>
+            <List spacing={3} textAlign="start">
+                {options.map((desc, id) => (
+                    <ListItem key={desc.id} fontSize={'1.12rem'}>
+                        <ListIcon as={FaCheckCircle} color="green.500" />
+                        {desc.desc}
+                    </ListItem>
+                ))}
+            </List>
+            <Heading size={'xl'}>{typePlan}</Heading>
+
+        </Stack>
+    );
+};
+const Pricing = () => {
+    return (
+        <Box bg="#000000" color="#ffffff">
+            <Navbar />
+            <Box py={12} px={5} minH={'100vh'}>
+                <Stack spacing={4} width={'100%'} direction={'column'}>
+                    <Stack
+                        p={5}
+                        alignItems={'center'}
+                        justifyContent={{
+                            base: 'flex-start',
+                            md: 'space-around',
+                        }}
+                        direction={{
+                            base: 'column',
+                            md: 'column',
+                        }}>
+                        <Stack
+                            width={{
+                                base: '100%',
+                                md: '40%',
+                            }}
+                            textAlign={'center'}>
+                            <Heading size={'2xl'}>
+                                The Right Plan for <Text color="purple.400">Your Business</Text>
+                            </Heading>
+                        </Stack>
+                        <Stack
+                            width={{
+                                base: '100%',
+                                md: '60%',
+                            }}
+                            py={{
+                                base: '1rem',
+                                md: '1.5rem',
+                            }}
+                        >
+                            <Text fontSize={'1.12rem'} textAlign={'center'}>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                                quod in iure vero. Facilis magnam, sed officiis commodi labore
+                                odit.
+                            </Text>
+                        </Stack>
+                    </Stack>
+                    <Divider />
+                    <PackageTier title={'Starter'} typePlan="Free" options={options} />
+                    <Divider />
+                    <PackageTier
+                        title={'Lorem Plus'}
+                        checked={true}
+                        typePlan="$32.00"
+                        options={options}
+                    />
+                    <Divider />
+                    <PackageTier title={'Lorem Pro'} typePlan="$50.00" options={options} />
+                </Stack>
+            </Box>
+        </Box>
+    );
+};
+
+export default Pricing;
