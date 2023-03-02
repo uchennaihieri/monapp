17ff5211bfedc9b9f34db2d31424a62ec210dae8
uchenna ihieri <uchennaihieri@yahoo.com>
2023-03-02 11:12:20 +0100

    update keys

--- services/initAuth.js
+++ services/initAuth.js
@@ -15,7 +15,7 @@ const initAuth = () => {
         firebaseAdminInitConfig: {
             credential: {
                 projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-                clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
                 // The private key must not be accessible on the client side.
                 privateKey: process.env.FIREBASE_PRIVATE_KEY,
             },
